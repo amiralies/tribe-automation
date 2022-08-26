34c623453f81ea5f2cf428bcbc36978e1b5bf6df
Amirali Esmaeili <esmailiamirali@gmail.com>
2022-08-27 04:01:44 +0430

    Update app entrypoint

--- src/main/scala/so/tribe/automation/Main.scala
+++ src/main/scala/so/tribe/automation/Main.scala
@@ -1,11 +1,54 @@
 package so.tribe.automation
 
 import zio._
-import zio.Console._
+import akka.actor.ActorSystem
+import akka.http.interop.HttpServer
+import so.tribe.automation.config.ConfigService
+import so.tribe.automation.config.ConfigServiceImpl
+import so.tribe.automation.config.HttpConfig
+import so.tribe.automation.healthcheck.HealthCheckRoutesImpl
+import so.tribe.automation.webhook.WebhookRoutesImpl
 
-object MyApp extends ZIOAppDefault {
+object Main extends ZIOAppDefault {
+  val httpServerLayer = {
+    val actorSystemLayer = ZLayer.scoped(
+      ZIO.acquireRelease(
+        ZIO.attempt(ActorSystem("TribeAutomationActorSystem"))
+      )(sys =>
+        ZIO
+          .fromFuture(_ => sys.terminate())
+          .either
+      )
+    )
 
-  override def run = logic
+    val routeLayer = ZLayer.service[AppRouter].project(_.routes)
 
-  val logic = printLine("Hello world")
+    val httpConfigLayer = ZLayer
+      .service[ConfigService]
+      .project(configService =>
+        HttpServer.Config(
+          configService.access.http.host,
+          configService.access.http.port
+        )
+      )
+
+    (actorSystemLayer ++ routeLayer ++ httpConfigLayer) >>> HttpServer.live
+  }
+
+  def startServer = ZIO.scoped {
+    for {
+      serverBinding <- HttpServer.start
+      () <- ZIO.logInfo(s"Server started at $serverBinding")
+      _ <- ZIO.never
+    } yield ()
+  }
+
+  override def run =
+    startServer.provide(
+      ConfigServiceImpl.layer,
+      WebhookRoutesImpl.layer,
+      HealthCheckRoutesImpl.layer,
+      AppRouterImpl.layer,
+      httpServerLayer
+    )
 }
