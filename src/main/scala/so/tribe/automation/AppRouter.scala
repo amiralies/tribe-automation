2a337c96a74b3e6131214d9c47ddd58d4336db77
Amirali Esmaeili <esmailiamirali@gmail.com>
2022-08-27 05:11:37 +0430

    Add akka exception handler

--- src/main/scala/so/tribe/automation/AppRouter.scala
+++ src/main/scala/so/tribe/automation/AppRouter.scala
@@ -5,6 +5,9 @@ import so.tribe.automation.webhook.WebhookRoutes
 import zio._
 import akka.http.scaladsl.server.Directives._
 import so.tribe.automation.healthcheck.HealthCheckRoutes
+import akka.http.scaladsl.server.ExceptionHandler
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.interop.ZIOSupport
 
 trait AppRouter {
   def routes: Route
@@ -13,11 +16,24 @@ trait AppRouter {
 case class AppRouterImpl(
     webhookRoutes: WebhookRoutes,
     healthCheckRoutes: HealthCheckRoutes
-) extends AppRouter {
-  override def routes = concat(
+) extends AppRouter
+    with ZIOSupport {
+  val rawRoutes = concat(
     healthCheckRoutes.routes,
     webhookRoutes.routes
   )
+  override def routes: Route = {
+
+    implicit def exceptionHandler: ExceptionHandler = {
+      ExceptionHandler { case e: Throwable =>
+        complete(
+          ZIO.logError(e.getMessage()).map(_ => StatusCodes.InternalServerError)
+        )
+      }
+    }
+
+    Route.seal(rawRoutes)
+  }
 }
 
 object AppRouterImpl {
